83b58b6bc128cf4a371a26f361c8d7f7e08a6641
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-11-02 15:35:52 -0300

    initial commit

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center>
+      <Text color="white" fontSize={24}>
+        Open up App.js to start working on your app!
+      </Text>
+    </Center>
+  );
+}
